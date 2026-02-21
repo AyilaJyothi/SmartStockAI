8217ca07444f1ba2716a4e05a15a2aa997d3ecae
DedeepyaKalavakuri <119920060+dedeepya55@users.noreply.github.com>
2026-02-21 20:04:47 +0530

    Add preview configuration to vite.config.js
    Added preview configuration to allow specific hosts and set port.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true, // allows all hosts
+    port: process.env.PORT || 5173,
+    allowedHosts: ["smartstock-frontend-pud5.onrender.com"] // allow Render domain
+  }
+});
